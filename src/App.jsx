3337a6dc90d2610dd34eedf335f3f5da41695ad0
Usama Khan <usamakhan111se@gmail.com>
2026-01-31 15:49:15 +0500

    mainjsx cleanup

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './Layout/Layout'
+import Home from './Layout/Home';
+import About from './Layout/About';
+import Contact from './Layout/Contact';
+import Login from './Layout/Components/Auth/Login';
+import Signup from './Layout/Components/Auth/signup';
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default App;   
--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,8 @@
 
 import { createRoot } from 'react-dom/client'
-import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from './Layout/Layout'
-import Home from './Layout/Home';
-import About from './Layout/About';
-import Contact from './Layout/Contact';
-import Login from './Layout/Components/Auth/Login';
-import Signup from './Layout/Components/Auth/signup';
-
-
-
+import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
-
-
-   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <>
+    <App />
+  </>
 )
